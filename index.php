3565bead3751f06a5876609e5998843b402d5e94
Cu Janeway <cuj.janeway@hotmail.com>
2021-01-16 10:07:09 +0000

    mo changes.

--- index.php
+++ index.php
@@ -7,28 +7,28 @@
         <link rel="stylesheet" href="styles/main.css">
         <link rel="icon" href="assets/favicon.png">
         <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"> 
     </head>
     <body>
     <?php include('includes/header.php'); ?>
-    <div class="pink-bg pink-bg--right"></div>
-    <main>
+    <main class="container">
+        <div class="pink-bg pink-bg--right"></div>
         <section class="me">
-            <h1 class="me__page-title">Cu Jane                way</h1>
+            <h1 class="me__page-title bold">Cu Janeway</h1>
             <img class="me__image" src="assets/me.webp">
-            <p class="me__subtitle color-japanese-laurel">Web Development</p>
-            <p class="me_description">Hey there, I'm a Web Developer from Manchester. Why don't you check out some of my work and doodles across the site.</p>
+            <p class="me__subtitle color-japanese-laurel semi-bold">Web Development</p>
+            <p class="me__description">Hey there, I'm a Web Developer from Manchester. Why don't you check out some of my work and doodles across the site.</p>
         </section>
         <section class="recent-posts">
-            <h2 class="recent-posts__title"></h2>
+            <h2 class="recent-posts__title">Recent Posts</h2>
             <ul class="recent-posts__list">
-                <li class="recent-posts__list-item">Lorum ipsum dulce et decorum est</li>
-                <li class="recent-posts__list-item">Lorum ipsum dulce et decorum est</li>
-                <li class="recent-posts__list-item">Lorum ipsum dulce et decorum est</li>
-                <li class="recent-posts__list-item">Lorum ipsum dulce et decorum est</li>
+                <li class="recent-posts__list-item"> Lorum ipsum dulce et decorum est dulce et decorum est</li>
+                <li class="recent-posts__list-item"> Lorum ipsum dulce et decorum est</li>
+                <li class="recent-posts__list-item"> Lorum ipsum dulce et decorum est</li>
+                <li class="recent-posts__list-item"> Lorum ipsum dulce et decorum est</li>
             </ul>
         </section>
-        <img class="doodle doodle--1" src="assets/illustrations/doodle1.svg">
+        <img class="doodle doodle--home" src="assets/illustrations/doodle1.svg">
     </main>
     <?php include('includes/footer.php'); ?>
     </body>
